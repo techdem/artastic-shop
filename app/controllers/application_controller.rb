935429c886ebf61edfca8c18a3cdd8cf249b6c2a
techdem <tudor.chiribes@gmail.com>
2018-08-01 08:05:37 +0000

    added recent item history

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,12 @@ class ApplicationController < ActionController::Base
   before_action :init
   
   def after_sign_in_path_for(resource)    # Redirect user to items page after login
-    :items
+    :root
   end
   
   def init
+    @history = Item.all
+    @ordered = Orderitem.all
     if session[:cart] then
       @cart = session[:cart]
     else
